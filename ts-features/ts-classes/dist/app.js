7a1a91024a9a7ebcd608d9670c0f34cb7bd59138
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2020-10-08 15:10:35 +0530

    constructor() function and the `this` keyword (#1)

--- ts-features/ts-classes/dist/app.js
+++ ts-features/ts-classes/dist/app.js
@@ -3,9 +3,14 @@ var Department = (function () {
     function Department(name) {
         this.name = name;
     }
+    Department.prototype.describe = function () {
+        console.log('Department: ' + this.name);
+    };
     return Department;
 }());
 ;
 var accounting = new Department('Accounting');
-console.log(accounting);
+accounting.describe();
+var accountingCopy = { describe: accounting.describe };
+accountingCopy.describe();
 //# sourceMappingURL=app.js.map
--- ts-features/ts-classes/src/app.ts
+++ ts-features/ts-classes/src/app.ts
@@ -11,17 +11,39 @@ class Department {
   constructor(name: string) {
     this.name = name;
   }
+
+  // adding a method in the class
+  describe(): void {
+    // console.log('Department: ' + name); // 'Department: undefined' --- since we are trying to access `window.name`
+    console.log('Department: ' + this.name); // 'Department: Accounting'
+  }
 };
 
 // creating an instance of the Department class
 const accounting = new Department('Accounting');
+accounting.describe();
 
-console.log(accounting);
+/**
+ * The `this` keyword can be tricky to understand, and so, if
+ * we want to, we can make a copy of the `describe()` method 
+ * inside the Department class using another object as follows
+ */
+const accountingCopy = { describe: accounting.describe };
+accountingCopy.describe(); // 'Department: undefined' --- because accountingCopy has no `name` property
+
+/**
+ * The `this` has not been enforced to check for what object 
+ * is calling the `describe()` method, and so, we can basically
+ * call describe() from `accountingCopy` object too. 
+ * 
+ * To avoid this, we have to pass in the `this` object for the
+ * describe() method with a type annotation of 'Department',
+ * which we'll see next.
+ */
 
 /**
  * Output
  * ------
- * Department
- *  > name: "Accounting"
- *  > proto: Object
+ * Department: Accounting
+ * Department: undefined
  */
