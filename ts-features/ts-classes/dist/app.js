e0a0e2ca1c4aa5f7ef3a693c4079db8477b87d8f
Chandrabhatta Sriram <sriram.ch1995@gmail.com>
2020-10-16 17:02:49 +0530

    Using Interfaces with Classes

--- ts-features/ts-classes/dist/app.js
+++ ts-features/ts-classes/dist/app.js
@@ -1,115 +1,28 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
-var Department = (function () {
-    function Department(id, name) {
-        this.id = id;
+;
+var user1;
+user1 = {
+    name: 'Ram',
+    age: 30,
+    greet: function (phrase) {
+        console.log(phrase + ' ' + this.name);
+    },
+};
+user1.greet('Hello -- I am');
+var Person = (function () {
+    function Person(name) {
         this.name = name;
-        this.employees = [];
-        console.log(Department.fiscalYear);
+        this.age = 30;
     }
-    Department.createEmployee = function (name) {
-        return { name: name };
+    Person.prototype.greet = function (phrase) {
+        console.log(phrase + ' ' + this.name);
     };
-    Department.prototype.addEmployee = function (employee) {
-        this.employees.push(employee);
-    };
-    Department.prototype.printEmployeeInformation = function () {
-        console.log('Number of Employees: ' + this.employees.length);
-        console.log(this.employees);
-    };
-    Department.fiscalYear = 2020;
-    return Department;
+    return Person;
 }());
 ;
-var employee1 = Department.createEmployee('Roop');
-console.log(employee1, Department.fiscalYear);
-var ITDepartment = (function (_super) {
-    __extends(ITDepartment, _super);
-    function ITDepartment(id, admins) {
-        if (admins === void 0) { admins = []; }
-        var _this = _super.call(this, id, 'IT') || this;
-        _this.admins = admins;
-        return _this;
-    }
-    ITDepartment.prototype.describe = function () {
-        console.log('IT Department -- ID: ' + this.id);
-    };
-    return ITDepartment;
-}(Department));
-var it = new ITDepartment('D2', ['Max', 'Manu']);
-it.describe();
-it.addEmployee('Ram');
-it.addEmployee('Max');
-it.printEmployeeInformation();
-console.log(it);
-var AccountingDepartment = (function (_super) {
-    __extends(AccountingDepartment, _super);
-    function AccountingDepartment(id, reports) {
-        if (reports === void 0) { reports = []; }
-        var _this = _super.call(this, id, 'Accounting') || this;
-        _this.reports = reports;
-        _this.lastReport = reports[0];
-        return _this;
-    }
-    AccountingDepartment.getInstance = function () {
-        if (this.instance)
-            return this.instance;
-        this.instance = new AccountingDepartment('D2');
-        return this.instance;
-    };
-    Object.defineProperty(AccountingDepartment.prototype, "mostRecentReport", {
-        get: function () {
-            if (!this.lastReport)
-                throw new Error('No report found');
-            return this.lastReport;
-        },
-        set: function (value) {
-            if (!value)
-                throw new Error('Please pass in a valid value');
-            this.addReport(value);
-        },
-        enumerable: false,
-        configurable: true
-    });
-    AccountingDepartment.prototype.describe = function () {
-        console.log('Accounting Department - ID: ' + this.id);
-    };
-    AccountingDepartment.prototype.addEmployee = function (employee) {
-        if (name === 'Max')
-            return;
-        this.employees.push(employee);
-    };
-    AccountingDepartment.prototype.addReport = function (text) {
-        this.reports.push(text);
-        this.lastReport = text;
-    };
-    AccountingDepartment.prototype.printReports = function () {
-        console.log(this.reports);
-    };
-    return AccountingDepartment;
-}(Department));
-var accounting = AccountingDepartment.getInstance();
-var accounting2 = AccountingDepartment.getInstance();
-console.log(accounting === accounting2);
-accounting.addReport('Something went wrong!!');
-accounting.addReport('Error rectified.');
-accounting.mostRecentReport = 'Year End Report';
-console.log(accounting.mostRecentReport);
-accounting.printReports();
-accounting.addEmployee('Max');
-accounting.addEmployee('Ram');
-accounting.printEmployeeInformation();
-accounting.describe();
+var user2 = new Person('Ram');
+user2.greet('Hi there - I am');
+var user3;
+user3 = new Person('Roop');
+user3.greet('Howdy! My name is');
 //# sourceMappingURL=app.js.map
--- ts-features/ts-classes/src/app.ts
+++ ts-features/ts-classes/src/app.ts
@@ -1,168 +1,60 @@
 /**
- * Assume that we are making an internal tooling for a company
- * which requires Department details and for that, we need to 
- * make a Department class.
+ * Interface can only be used to define an object whereas 
+ * custom types (using `type` keyword) can also be used to 
+ * defining arrays, union types, literal types, etc.
  */
 
-/**
- * There's a pattern in OOP which is called Singleton Pattern.
- * The singleton pattern ensures that there's only a single
- * instance available for the class, meaning, we might not
- * be able to make many instances of a single class, we can 
- * only make one instance of the class, and at the same time
- * we make sure that we can create no other instances of that
- * class.
- * 
- * To achieve this, we need to create a private constructor 
- * because of which, we can create the instance of the class
- * only from inside the class. To give access to the created 
- * instance, we can create a static method which returns the 
- * instance of the class, and that instance is also a static
- * property which is also private.
- * 
- * We can make sure that all this happens, only for the 
- * 'AccountingDepartment' class.
- */
-
-abstract class Department {
-  static fiscalYear = 2020;
-  protected employees: string[] = [];
-  
-  constructor(protected readonly id: string, public name: string) { 
-    console.log(Department.fiscalYear);
-  }
-
-  static createEmployee(name: string) {
-    return { name: name };
-  }
-
-  addEmployee(employee: string): void {
-    this.employees.push(employee);
-  }
-
-  printEmployeeInformation() {
-    console.log('Number of Employees: ' + this.employees.length);
-    console.log(this.employees);
-  }
-
-  // `abstract` methods don't have a body, they're just declaration(s)
-  abstract describe(this: Department): void;
+interface Person {
+  name: string;
+  age: number;
+  greet(phrase: string): void;
 };
 
-const employee1 = Department.createEmployee('Roop');
-console.log(employee1, Department.fiscalYear);
+let user1: Person;
 
-// class below extends 'Department' -- and so, 'ITDepartment' has to define all the abstract methods/properties
-class ITDepartment extends Department {
-  constructor(id: string, public admins: string[] = []) {
-    super(id, 'IT'); // super() has to be called first in the child class' constructor before going ahead further
-  }
+user1 = {
+  name: 'Ram',
+  age: 30,
+  greet(phrase: string) {
+    console.log(phrase + ' ' + this.name);
+  },
+};
 
-  describe(this: ITDepartment): void {
-    console.log('IT Department -- ID: ' + this.id);
-  }
+user1.greet('Hello -- I am');
+
+// If we make a 'Greetable' and implement it for a class, then that class has to implement all the properties/functions of that interface
+interface Greetable {
+  name: string;
+  greet(phrase: string): void;
 }
 
-const it = new ITDepartment('D2', ['Max', 'Manu']);
-it.describe();
-it.addEmployee('Ram');
-it.addEmployee('Max');
-it.printEmployeeInformation();
-console.log(it);
+// a class can implement any number of interfaces using the following syntax
+// interface A {/* some definitions */}
+// interface B {/* some definitions */}
+// class C implements A, B {/* some definitions */}
 
-class AccountingDepartment extends Department {
-  private lastReport: string;
-  private static instance: AccountingDepartment;
+class Person implements Greetable {
+  // name: string; // adding because of the 'Greetable' interface
+  age = 30; // this class' property (interface doesn't matter)
+
+  constructor(public name: string) {}
   
-  // if we make this `private`, we can only call it from inside this class
-  private constructor(id: string, private reports: string[] = []) {
-    super(id, 'Accounting');
-    this.lastReport = reports[0];
+  greet(phrase: string): void {
+    console.log(phrase + ' ' + this.name);
   }
+};
 
-  // make a static method that returns the instance for 'AccountingDepartment'
-  static getInstance() {
-    if (this.instance) return this.instance; // return AccountingDepartment.instance
-    this.instance = new AccountingDepartment('D2');
-    return this.instance; // return AccountingDepartment.instance
-  }
+const user2 = new Person('Ram');
+user2.greet('Hi there - I am');
 
-  // getter using `get` method
-  get mostRecentReport() {
-    if (!this.lastReport)
-      throw new Error('No report found');
-    return this.lastReport;
-  }
-
-  // we can use the same name for getter/setter to read/set a value as follows
-  set mostRecentReport(value: string) {
-    if (!value) throw new Error('Please pass in a valid value');
-    this.addReport(value);
-  }
-
-  // overriding describe() method
-  describe() {
-    console.log('Accounting Department - ID: ' + this.id); // change `id` field to `protected` to be accessible by the child classes
-  }
-
-  // overriding addEmployee
-  addEmployee(employee: string): void {
-    if (name === 'Max') return;
-    this.employees.push(employee);
-  }
-
-  addReport(text: string) {
-    this.reports.push(text);
-    this.lastReport = text;
-  }
-
-  printReports() {
-    console.log(this.reports);
-  }
-}
-
-// const accounting = new AccountingDepartment('D3'); // error: Constructor of class 'AccountingDepartment' is private and only accessible within the class declaration.ts(2673)
-
-const accounting = AccountingDepartment.getInstance();
-const accounting2 = AccountingDepartment.getInstance();
-console.log(accounting === accounting2); // true -- because 'AccountingDepartment' is a singleton class
-
-accounting.addReport('Something went wrong!!');
-accounting.addReport('Error rectified.');
-
-// accessing setter as a property
-accounting.mostRecentReport = 'Year End Report';
-
-// accessing getter also as a property
-console.log(accounting.mostRecentReport); // after adding a report, we won't get any error
-
-accounting.printReports();
-
-accounting.addEmployee('Max'); // will not be added
-accounting.addEmployee('Ram'); // will be added
-accounting.printEmployeeInformation();
-
-accounting.describe();
-
+let user3: Greetable;
+user3 = new Person('Roop'); // this is valid, as 'Person' implements 'Greetable' interface
+user3.greet('Howdy! My name is');
 
 /**
  * Output
  * ------
- * {name: "Roop"} 2020
- * 2020
- * IT Department -- ID: D2
- * Number of Employees: 2
- * > ["Ram", "Max"]
- * > ITDepartment
- *    > admins: ["Max", "Manu"]
- *    > employees: ["Ram", "Max"]
- *    id: "D2"
- *    name: "IT"
- * 2020
- * true
- * Year End Report
- * > ["Something went wrong!!", "Error rectified.", "Year End Report"]
- * Number of Employees: 2
- * > ["Max", "Ram"]
- * Accounting Department - ID: D2
+ * Hello -- I am Ram
+ * Hi there - I am Ram
+ * Howdy! My name is Roop
  */
